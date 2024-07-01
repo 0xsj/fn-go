e4b63dd85e90f9f4d2e28370b22fbe9bae814b41
sjtommylee <sjtommylee@gmail.com>
2024-07-01 11:06:02 -0700

    basic auth pages, form

---
+++ src/pages/auth/modules/sign-up.tsx
@@ -0,0 +1,12 @@
+import { SignUpForm } from "../components/sign-up-form";
+
+export default function SignUp() {
+  return (
+    <>
+      <div>
+        <h1>Sign Up</h1>
+        <SignUpForm />
+      </div>
+    </>
+  );
+}
