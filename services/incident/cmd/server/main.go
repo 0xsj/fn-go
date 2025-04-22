b63ed7d8d04b4211c8d8dacbcc41387fad5b0446
0xsj <sjtommylee@gmail.com>
2025-04-22 06:05:32 -0700

    protocol buffer definitions

--- services/incident/cmd/server/main.go
+++ services/incident/cmd/server/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"github.com/0xsj/fn-go/pkg/common/logging"
+)
+
+func main() {
+	logger := logging.NewSimpleLogger(logging.InfoLevel)
+	logger.Debug("This is a debug message") 
+	logger.Info("Starting address service")
+	logger.Info("Testing structured logging", logging.F("service", "address"))
+	
+	serviceLogger := logger.With(logging.F("service", "address"), logging.F("version", "1.0.0"))
+	serviceLogger.Info("Using logger with fields")
+	serviceLogger.Warn("This is a warning with fields")
+	
+	serviceLogger.Error("An error occurred", logging.F("error_code", 500))
+}
