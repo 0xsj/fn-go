ba92852005331e350bc574fe11f1b465d2e0e2fd
0xsj <sjtommylee@gmail.com>
2025-05-01 22:38:00 -0700

    configuration

--- services/users/cmd/server/main.go
+++ services/users/cmd/server/main.go
@@ -1,18 +1,16 @@
 package main
 
 import (
+	"github.com/0xsj/fn-go/pkg/common/config"
 	"github.com/0xsj/fn-go/pkg/common/logging"
+	userConfig "github.com/0xsj/fn-go/services/users/internal/config"
 )
 
 func main() {
+	envProvider := config.NewEnvProvider("USERS")
+	cfg := userConfig.LoadConfig(envProvider)
+
 	logger := logging.NewSimpleLogger(logging.InfoLevel)
-	logger.Debug("This is a debug message") 
-	logger.Info("Starting address service")
-	logger.Info("Testing structured logging", logging.F("service", "address"))
-	
-	serviceLogger := logger.With(logging.F("service", "address"), logging.F("version", "1.0.0"))
-	serviceLogger.Info("Using logger with fields")
-	serviceLogger.Warn("This is a warning with fields")
-	
-	serviceLogger.Error("An error occurred", logging.F("error_code", 500))
+
+
 }
--- services/users/internal/handler/grpc.go
+++ services/users/internal/handler/grpc.go
@@ -1,1 +1,22 @@
 package handler
+
+import (
+	"log"
+
+	"github.com/0xsj/fn-go/pkg/proto/users"
+	"github.com/0xsj/fn-go/services/users/internal/config"
+)
+
+type UserServiceHandler struct {
+	users.UnimplementedUserServiceServer
+	cfg *config.Config
+	logger *log.Logger
+}
+
+func NewServiceHandler(cfg *config.Config, logger *log.Logger) *UserServiceHandler {
+	return &UserServiceHandler{
+		cfg: cfg,
+		logger: logger,
+	}
+}
+
