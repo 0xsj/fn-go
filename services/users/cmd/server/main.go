b5d973b649596300ad7fea003b0477a0ca35b80a
0xsj <sjtommylee@gmail.com>
2025-05-01 23:06:27 -0700

    clean up

--- services/users/cmd/server/main.go
+++ services/users/cmd/server/main.go
@@ -12,31 +12,61 @@ import (
 	"google.golang.org/grpc"
 )
 
+// func main() {
+// 	envProvider := config.NewEnvProvider("USERS")
+// 	cfg := userConfig.LoadConfig(envProvider)
+
+// 	baseLogger := logging.NewSimpleLogger(logging.InfoLevel)
+// 	logger := baseLogger.With(logging.F("service", "users"))
+// 	logger.Info("Starting users service")
+
+// 	server := grpc.NewServer()
+
+// 	userHandler := handler.NewUserServiceHandler(cfg, logger)
+// 	users.RegisterUserServiceServer(server, userHandler)
+
+// 	port := cfg.Server.Port
+// 	if port == 0 {
+// 		port = 50051
+// 	}
+
+// 	logger.Info("Users service listening", logging.F("port", port))
+// 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+// 	if err != nil {
+// 		logger.Fatal("Failed to listen", logging.F("error", err))
+// 	}
+
+// 	if err := server.Serve(lis); err !=  nil {
+// 		logger.Fatal("Failed to serve", logging.F("error", err))
+// 	}
+// }
+
+
 func main() {
-	envProvider := config.NewEnvProvider("USERS")
-	cfg := userConfig.LoadConfig(envProvider)
+    envProvider := config.NewEnvProvider("USERS")
+    cfg := userConfig.LoadConfig(envProvider)
 
-	baseLogger := logging.NewSimpleLogger(logging.InfoLevel)
-	logger := baseLogger.With(logging.F("service", "users"))
-	logger.Info("Starting users service")
+    baseLogger := logging.NewSimpleLogger(logging.InfoLevel)
+    serviceLogger := baseLogger.With(logging.F("service", "users"))
+    serviceLogger.Info("Starting users service")
 
-	server := grpc.NewServer()
+    server := grpc.NewServer()
 
-	userHandler := handler.NewUserServiceHandler(cfg, logger)
-	users.RegisterUserServiceServer(server, userHandler)
+    userHandler := handler.NewUserServiceHandler(cfg, serviceLogger)
+    users.RegisterUserServiceServer(server, userHandler)
 
-	port := cfg.Server.Port
-	if port == 0 {
-		port = 50051
-	}
-
-	logger.Info("Users service listening", logging.F("port", port))
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
-	if err != nil {
-		logger.Fatal("Failed to listen", logging.F("error", err))
-	}
-
-	if err := server.Serve(lis); err !=  nil {
-		logger.Fatal("Failed to serve", logging.F("error", err))
-	}
+    port := cfg.Server.Port
+    if port == 0 {
+        port = 50051 
+    }
+    serviceLogger.Info("Users service listening", logging.F("port", port))
+    
+    lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+    if err != nil {
+        serviceLogger.Fatal("Failed to listen", logging.F("error", err))
+    }
+    
+    if err := server.Serve(lis); err != nil {
+        serviceLogger.Fatal("Failed to serve", logging.F("error", err))
+    }
 }
