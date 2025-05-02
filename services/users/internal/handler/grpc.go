f599323004edd9bbe646dd078da73a354209f7c2
0xsj <sjtommylee@gmail.com>
2025-05-01 22:47:43 -0700

    simple grpc user handler

--- services/users/internal/handler/grpc.go
+++ services/users/internal/handler/grpc.go
@@ -1,9 +1,13 @@
 package handler
 
 import (
+	"context"
+
 	"github.com/0xsj/fn-go/pkg/common/logging"
 	"github.com/0xsj/fn-go/pkg/proto/users"
 	"github.com/0xsj/fn-go/services/users/internal/config"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type UserServiceHandler struct {
@@ -19,3 +23,20 @@ func NewUserServiceHandler(cfg *config.Config, logger logging.Logger) *UserServi
 	}
 }
 
+func (h *UserServiceHandler) GetUser(ctx context.Context, req *users.GetUserRequest) (*users.GetUserResponse, error) {
+	h.logger.Info("GetUser request received", logging.F("id", req.Id))
+	if req.Id == "" {
+		return nil, status.Error(codes.InvalidArgument, "user ID is required")
+	}
+
+	user := &users.User{
+		Id: req.Id,
+		Email: "user@example.com",
+		Name: "Example User",
+		Role: "user",
+	}
+
+	return &users.GetUserResponse{
+		User: user,
+	}, nil
+}
