0362d2700e8d90c32d48e374de29cb911b0264d0
0xsj <sjtommylee@gmail.com>
2025-05-11 02:07:09 -0700

    fix dockerfile

--- services/location-service/cmd/server/main.go
+++ services/location-service/cmd/server/main.go
@@ -1,1 +1,12 @@
-package server
+package main
+
+import (
+	"github.com/0xsj/fn-go/pkg/common/log"
+)
+
+
+func main(){
+	logger := log.Default()
+	logger = logger.WithLayer("location-service")
+	logger.Info("Initializing location service")
+}
--- services/notification-service/cmd/server/main.go
+++ services/notification-service/cmd/server/main.go
@@ -1,1 +1,12 @@
-package server
+package main
+
+import (
+	"github.com/0xsj/fn-go/pkg/common/log"
+)
+
+
+func main(){
+	logger := log.Default()
+	logger = logger.WithLayer("notification-service")
+	logger.Info("Initializing notification service")
+}
