2d61178e0a29c9f6832de652ad8b37823d7f85c0
0xsj <sjtommylee@gmail.com>
2025-05-10 23:22:47 -0700

    user

--- pkg/models/user.go
+++ pkg/models/user.go
@@ -26,6 +26,39 @@ type User struct {
 	DeletedAt *time.Time `json:"deleted_at,omitempty"`
 }
 
+type UserCreateRequest struct {
+    Username  string `json:"username" validate:"required"`
+    Email     string `json:"email" validate:"required,email"`
+    Password  string `json:"password" validate:"required,min=8"`
+    FirstName string `json:"first_name"`
+    LastName  string `json:"last_name"`
+    Phone     string `json:"phone"`
+    Role      Role   `json:"role" validate:"required,oneof=admin customer dispatcher"`
+}
+
+type UserUpdateRequest struct {
+    Username  *string `json:"username,omitempty"`
+    Email     *string `json:"email,omitempty" validate:"omitempty,email"`
+    FirstName *string `json:"first_name,omitempty"`
+    LastName  *string `json:"last_name,omitempty"`
+    Phone     *string `json:"phone,omitempty"`
+    Role      *Role   `json:"role,omitempty" validate:"omitempty,oneof=admin customer dispatcher"`
+    Active    *bool   `json:"active,omitempty"`
+}
+
+type UserCredentials struct {
+    Username string `json:"username"`
+    Password string `json:"password"`
+}
+
+type UserSummary struct {
+    ID        string `json:"id"`
+    Username  string `json:"username"`
+    FirstName string `json:"first_name"`
+    LastName  string `json:"last_name"`
+    Role      Role   `json:"role"`
+}
+
 type UserLookupRequest struct {
     ID string `json:"id"`
 }
